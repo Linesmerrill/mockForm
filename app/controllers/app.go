b849bf94633c8af831e81c20eadc40d9c2a1db44
localadmin <localadmin@compozed-tss-15.mobile.asu.edu>
2016-07-25 11:45:46 -0700

    working form

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -3,26 +3,27 @@ package controllers
 import (
 	"github.com/revel/revel"
 	"fmt"
+	"net/http"
+	// "html"
+	// "encoding/json"
 )
 
 type App struct {
 	*revel.Controller
 }
 
+type Surface struct {
+    Data string `json:"data"` //since we have to export the field
+                              //but want the lowercase letter
+}
+
 func (c App) Index() revel.Result {
 	return c.Render()
 }
 
-func (c App) Verify() revel.Result {
-	// fmt.Println(form)
-	// ParseParams(c.Params, c.Request)
-	// url.Parse(c.Params)
-	// for k,v := range MAPP{
-	// 	fmt.Println("Key:", k,"Value:", v)
-	// }
+func (c App) Verify(req *http.Request) revel.Result {
 	
-	// fmt.Println("THIS IS THE REQUEST", c.Request)
-	fmt.Println("THIS IS THE PARAMS", c.Params)
+	fmt.Println("THIS IS THE PARAMS", c.Params.Form.Get("serviceName"))
 	return c.Render()
 }
 
