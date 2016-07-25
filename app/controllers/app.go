ca34f41461a85dde6db4628a8e2718d600253c37
localadmin <localadmin@compozed-tss-06.allstate.com>
2016-07-25 14:44:36 -0700

    I am a silly goose. Here are the rest of the files for basic back end validation

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/revel/revel"
 	"fmt"
+	// "reflect"
 	// "net/http"
 	// "html"
 	// "encoding/json"
@@ -18,22 +19,19 @@ type Surface struct {
 }
 
 func (c App) Index() revel.Result {
+
 	return c.Render()
 }
 
 
 func (c App) Verify() revel.Result {
-	// fmt.Println(form)
-	// ParseParams(c.Params, c.Request)
-	// url.Parse(c.Params)
-	// for k,v := range MAPP{
-	// 	fmt.Println("Key:", k,"Value:", v)
-	// }
+	params := c.Params.Form
 
-	// fmt.Println("THIS IS THE REQUEST", c.Request)
-	fmt.Println("THIS IS THE PARAMS", c.Params)
-	c.FlashParams()
-	params := c.Params.Form.Get("addInstruct")
+	for k := range c.Params.Form {
+		if(c.VerifyUserInput(params[k][0])){
+			return c.Redirect("/")
+		}
+	}
 	return c.Render(params)
 }
 
@@ -41,7 +39,11 @@ func (c App) Blah() revel.Result {
 	fmt.Println("AHHHHHHHHH STUFFFFFF", c.Params)
 	return c.Render()
 }
-func (c App) RouteToVerifyPage() revel.Result {
-	fmt.Println(c)
-	return c.Redirect(App.Blah)
+
+func (c App) VerifyUserInput(form string) bool{
+		c.Validation.Required(form)
+		if(c.Validation.HasErrors()){
+			return true
+		}
+		return false
 }
