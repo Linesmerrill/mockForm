3935a681ea67e743e48eb1bd34ed89ba6c0316cc
localadmin <localadmin@compozed-tss-06.allstate.com>
2016-07-25 14:43:51 -0700

    Created basic back end verification with for loop

--- tests/mockForm_test.go
+++ tests/mockForm_test.go
@@ -33,8 +33,16 @@ var _ = Describe("MockForm", func() {
 	})
 	It("Should have a submit button and route to a new page", func(){
 		Expect(page.Navigate("http://localhost:9000")).To(Succeed())
+		Expect(page.Find(".serviceName").Fill("Test1")).To(Succeed())
+		Expect(page.Find(".backendServices").Fill("Java")).To(Succeed())
 		Expect(page.Find(".submitBtn").Click()).To(Succeed())
 		Expect(page).To(HaveURL("http://localhost:9000/verify"))
+	})
+
+	It("Should not Redirect when submit Button pressed given an empty field", func(){
+		Expect(page.Navigate("http://localhost:9000")).To(Succeed())
+		Expect(page.Find(".submitBtn").Click()).To(Succeed())
+		Expect(page).To(HaveURL("http://localhost:9000/"))
 	})
 
 })
