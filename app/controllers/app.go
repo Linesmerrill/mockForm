61e152f7db7734fb374598f7df11a938db40c716
localadmin <localadmin@compozed-tss-06.mobile.asu.edu>
2016-07-25 11:50:27 -0700

    D&C

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"github.com/revel/revel"
 	"fmt"
-	"net/http"
+	// "net/http"
 	// "html"
 	// "encoding/json"
 )
@@ -21,13 +21,27 @@ func (c App) Index() revel.Result {
 	return c.Render()
 }
 
-func (c App) Verify(req *http.Request) revel.Result {
-	
-	fmt.Println("THIS IS THE PARAMS", c.Params.Form.Get("serviceName"))
-	return c.Render()
+
+func (c App) Verify() revel.Result {
+	// fmt.Println(form)
+	// ParseParams(c.Params, c.Request)
+	// url.Parse(c.Params)
+	// for k,v := range MAPP{
+	// 	fmt.Println("Key:", k,"Value:", v)
+	// }
+
+	// fmt.Println("THIS IS THE REQUEST", c.Request)
+	fmt.Println("THIS IS THE PARAMS", c.Params)
+	c.FlashParams()
+	params := c.Params.Form.Get("addInstruct")
+	return c.Render(params)
 }
 
+func (c App) Blah() revel.Result {
+	fmt.Println("AHHHHHHHHH STUFFFFFF", c.Params)
+	return c.Render()
+}
 func (c App) RouteToVerifyPage() revel.Result {
 	fmt.Println(c)
-	return c.Redirect(App.Verify)
+	return c.Redirect(App.Blah)
 }
